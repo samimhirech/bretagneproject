d8268536a0076925cf41043270653e0a17ba255c
Sami Mhirech <samimhirech@MacBook-Air-de-Sami.local>
2017-11-09 19:16:59 +0100

    Creation of dataset Routes bis

---
+++ packageapi/R/routes_doc.R
@@ -0,0 +1,7 @@
+#' Routes dataset
+#'
+#' @name routes
+#' @docType data
+#' @author Sami Mhirech \email{sami.mhirech@polytechnique.edu}
+#' @keywords data
+NULL
--- packageapi/data-raw/devtools_history.R
+++ packageapi/data-raw/devtools_history.R
@@ -1,3 +1,6 @@
 devtools::use_data_raw()
 
 devtools::use_package("jsonlite")
+
+routes <- jsonlite::fromJSON("https://applications002.brest-metropole.fr/WIPOD01/Transport/REST/getRoutes?format=json")
+devtools::use_data(routes)
