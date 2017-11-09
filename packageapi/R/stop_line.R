edf58ec0c8b8a700647e0d0fc57093c002681816
Sami Mhirech <samimhirech@MacBook-Air-de-Sami.local>
2017-11-09 19:00:08 +0100

    Function stop_line creation

--- packageapi/R/stop_line.R
+++ packageapi/R/stop_line.R
@@ -1,7 +1,6 @@
 #' stop_line
 #'
 #' @param line_id is a string (the line_id)
-#' @param destination is a string (the destination)
 #' @return the stops of a line
 #'
 #' @import jsonlite
@@ -10,8 +9,12 @@
 #'
 #' @examples
 
-stop_line <- function(line_id,destination) {
-    apiurl <- paste('https://applications002.brest-metropole.fr/WIPOD01/Transport/REST/getStops_route?format=json&route_id=',line_id,'&trip_headsign=',destination, sep="")
-    data <- jsonlite::fromJSON(apiurl)
-    data$Stop_name
+stop_line <- function(route_id) {
+  query <- paste("https://applications002.brest-metropole.fr/WIPOD01/Transport/REST/getDestinations?format=json&route_id=",route_id,"", sep = "")
+  headsigns <-jsonlite::fromJSON(query)
+  headsigns_list <- gsub(" ","%20",headsigns$Trip_headsign)
+  liste <- lapply(FUN = paste, "https://applications002.brest-metropole.fr/WIPOD01/Transport/REST/getStops_route?format=json&route_id=",route_id,"&trip_headsign=", headsigns_list, sep = "")
+  stops_list <- lapply(liste[[1]], FUN = jsonlite::fromJSON)
+  return(unique(Reduce(rbind, stops_list)$Stop_name))
 }
+
