a285d8dfb19dede1ee5687338fbeda960d23ec19
Sami Mhirech <samimhirech@MacBook-Air-de-Sami.local>
2017-11-13 23:41:07 +0100

    Creating the vignette of the package

--- packageapi/data-raw/devtools_history.R
+++ packageapi/data-raw/devtools_history.R
@@ -4,3 +4,6 @@ devtools::use_package("jsonlite")
 
 routes <- jsonlite::fromJSON("https://applications002.brest-metropole.fr/WIPOD01/Transport/REST/getRoutes?format=json")
 devtools::use_data(routes)
+
+devtools::use_vignette("packageapi")
+devtools::build_vignettes()
